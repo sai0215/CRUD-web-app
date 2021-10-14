8998879e7e1ef19e6ed2987179aacc9c31e13e40
Sai Vignesh <71221464+sai0215@users.noreply.github.com>
2021-10-14 21:15:52 +0530

    Add files via upload

---
+++ st.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `Student` (
+	`name` varchar(55) NOT NULL,
+    `rollno` int(8) NOT NULL,
+    `address` varchar(55) NOT NULL,
+	`branch` varchar(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
